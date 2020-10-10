3978db21a0f4f6282c4bab7816f3686eb9901068
Iván Velázquez <qievenz@users.noreply.github.com>
2020-10-10 16:29:41 -0300

    pgm

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PropertyGen
 {
@@ -6,7 +7,42 @@ namespace PropertyGen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var archivoEntrada = @"C:\GIT\PropertyGen\campos.txt";
+            var archivoSalida = @"C:\GIT\PropertyGen\salida.txt";
+
+            File.Delete(archivoSalida);
+            using (var sr = new StreamReader(archivoEntrada))
+            {
+                string linea;
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    var lineaPropiedad = linea.Split(";");
+                    var nombre = lineaPropiedad[0];
+                    var tipo = lineaPropiedad[1];
+                    var longitud = lineaPropiedad[2] ?? "";
+                    var nombrePrivado = $"_{char.ToLower(nombre[0]) + nombre.Substring(1)}";
+                    var propf = GenerarPropFull(nombre, nombrePrivado, tipo, longitud);
+
+                    Console.WriteLine(propf);
+                    File.AppendAllText(archivoSalida, propf);
+                }
+            }
+        }
+
+        private static string GenerarPropFull(string propName, string propPrivateName, string propType, string propLongitud)
+        {
+            var propf = $"private {propType} {propPrivateName};\n\n";
+
+            propf += $"public {propType} {propName}\n{{\n";
+            propf += $"\tget {{ return {propPrivateName}; }}\n";
+            propf += $"\tset\n\t{{\n\t";
+            propf += $"\tvar longitud = {propLongitud};\n\n";
+            propf += $"\t\t{propPrivateName} = value\n";
+            propf += $"\t\t\t.PadRight(longitud)\n";
+            propf += $"\t\t\t.Substring(0, longitud);\n";
+            propf += $"\t}}\n}}\n\n";
+
+            return propf;
         }
     }
 }
