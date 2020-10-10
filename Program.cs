f9a31a8b39a24eea93f98bbba0d8747bb3a5718d
Iván Velázquez <qievenz@users.noreply.github.com>
2020-10-10 16:15:16 -0300

    Inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PropertyGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
