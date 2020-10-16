1acc1f9dd0fa287957001ebf0afe74bc888f628d
Iván Velázquez <qievenz@users.noreply.github.com>
2020-10-16 10:12:31 -0300

    ints

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace PropertyGen
     {
         static void Main(string[] args)
         {
-            var archivoEntrada = @"C:\GIT\PropertyGen\campos.txt";
+            var archivoEntrada = @"C:\GIT\PropertyGen\PersonasFULL.txt";
+            //var archivoEntrada = @"C:\GIT\PropertyGen\RelacionesFULL.txt";
+            //var archivoEntrada = @"C:\GIT\PropertyGen\TransaccionesFULL.txt";
             var archivoSalida = @"C:\GIT\PropertyGen\salida.txt";
 
             File.Delete(archivoSalida);
@@ -18,10 +20,11 @@ namespace PropertyGen
                 {
                     var lineaPropiedad = linea.Split(";");
                     var nombre = lineaPropiedad[0];
-                    var tipo = lineaPropiedad[1];
-                    var longitud = lineaPropiedad[2] ?? "";
+                    var longitud = lineaPropiedad[1] ?? "";
+                    var decimales = lineaPropiedad[2];
+                    var tipo = lineaPropiedad[3];
                     var nombrePrivado = $"_{char.ToLower(nombre[0]) + nombre.Substring(1)}";
-                    var propf = GenerarPropFull(nombre, nombrePrivado, tipo, longitud);
+                    var propf = GenerarPropFull(nombre, nombrePrivado, tipo, longitud, decimales);
 
                     Console.WriteLine(propf);
                     File.AppendAllText(archivoSalida, propf);
@@ -29,18 +32,32 @@ namespace PropertyGen
             }
         }
 
-        private static string GenerarPropFull(string propName, string propPrivateName, string propType, string propLongitud)
+        private static string GenerarPropFull(string propName, string propPrivateName, string propType, string propLongitud, string propDecimal)
         {
-            var propf = $"private {propType} {propPrivateName};\n\n";
+            var propf = $"private string {propPrivateName};\n\n";
 
-            propf += $"public {propType} {propName}\n{{\n";
+            propf += $"public string {propName}\n{{\n";
             propf += $"\tget {{ return {propPrivateName}; }}\n";
             propf += $"\tset\n\t{{\n\t";
-            propf += $"\tvar longitud = {propLongitud};\n\n";
-            propf += $"\t\t{propPrivateName} = value\n";
-            propf += $"\t\t\t.PadRight(longitud)\n";
-            propf += $"\t\t\t.Substring(0, longitud);\n";
-            propf += $"\t}}\n}}\n\n";
+
+            if (propType == "string")
+            {
+                propf += $"\tvar longitud = {propLongitud};\n\n";
+                propf += $"\t\t{propPrivateName} = value\n";
+                propf += $"\t\t\t.PadRight(longitud)\n";
+                propf += $"\t\t\t.Substring(0, longitud);\n";
+                propf += $"\t}}\n}}\n\n";
+            }
+            if (propType == "int")
+            {
+                var longitud = Convert.ToInt32(propLongitud);
+                if (propDecimal != "0") longitud = Convert.ToInt32(propDecimal) + 1;
+                propf += $"\tvar longitud = {longitud};\n\n";
+                propf += $"\t\t{propPrivateName} = value\n";
+                propf += $"\t\t\t.PadLeft(longitud, '0')\n";
+                propf += $"\t\t\t.Substring(0, longitud);\n";
+                propf += $"\t}}\n}}\n\n";
+            }
 
             return propf;
         }
