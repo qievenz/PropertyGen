ba1e4416f9c98e82789bd24f5a21c78516d5370e
Iván Velázquez <qievenz@users.noreply.github.com>
2020-11-10 14:32:47 -0300

    IHostBuilder

---
+++ PropertyGenCore/Generador.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyGenCore
+{
+    public class Generador
+    {
+        private readonly string _template;
+
+        public Generador(string template)
+        {
+            _template = template;
+        }
+
+        public void GenerarArchivo(string rutaArchivoEntrada)
+        {
+            var archivoSalida = @"C:\GIT\PropertyGen\salida.txt";
+
+            File.Delete(archivoSalida);
+            using (var sr = new StreamReader(rutaArchivoEntrada))
+            {
+                string linea;
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    var lineaPropiedad = linea.Split(";");
+                    var nombre = lineaPropiedad[0];
+                    var longitud = lineaPropiedad[1] ?? "";
+                    var decimales = lineaPropiedad[2];
+                    var tipo = lineaPropiedad[3];
+                    var propf = GenerarPropGetFull(nombre, tipo, longitud, decimales);
+
+                    Console.WriteLine(propf);
+                    File.AppendAllText(archivoSalida, propf);
+                }
+            }
+        }
+
+        public string GenerarPropGetFull(string propName, string propType, string propLongitud, string propDecimal)
+        {
+            var propf = _template.Replace("MyProperty", propName);
+            propf = _template.Replace("myVar", "_" + char.ToLower(propName[0]) + propName.Substring(1));
+            propf = _template.Replace("myType", propType);
+
+
+
+            return propf;
+        }
+
+        public static string GenerarPropSetFull(string propName, string propPrivateName, string propType, string propLongitud, string propDecimal)
+        {
+            var propf = "";
+
+            if (propType == "string")
+            {
+                propf = $"private string {propPrivateName} = \" \";\n\n";
+
+                propf += $"public string {propName}\n{{\n";
+                propf += $"\tget {{ return {propPrivateName}; }}\n";
+                propf += $"\tset\n\t{{\n\t";
+
+                propf += $"\tvar longitud = {propLongitud};\n\n";
+                propf += $"\t\t{propPrivateName} = value\n";
+                propf += $"\t\t\t.PadRight(longitud)\n";
+                propf += $"\t\t\t.Substring(0, longitud)\n";
+                propf += $"\t\t\t.Trim();\n";
+                propf += $"\t}}\n}}\n\n";
+            }
+            if (propType == "int")
+            {
+                propf = $"private string {propPrivateName} = \"0\";\n\n";
+
+                propf += $"public string {propName}\n{{\n";
+                propf += $"\tget {{ return {propPrivateName}; }}\n";
+                propf += $"\tset\n\t{{\n\t";
+
+                var longitud = Convert.ToInt32(propLongitud);
+                if (propDecimal != "0") longitud = Convert.ToInt32(propDecimal) + 1;
+                propf += $"\tvar longitud = {longitud};\n\n";
+                propf += $"\t\t{propPrivateName} = value\n";
+                propf += $"\t\t\t.PadLeft(longitud, '0')\n";
+                propf += $"\t\t\t.Substring(0, longitud)\n";
+                propf += $"\t\t\t.Trim();\n";
+                propf += $"\t}}\n}}\n\n";
+            }
+
+            return propf;
+        }
+    }
+}
---
+++ PropertyGenCore/Startup.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PropertyGenCore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true);
+            var configuration = builder.Build();
+
+            var b = new Generador(configuration.GetValue<string>("Template1"));
+            b.GenerarArchivo("c:\\salida.txt");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
